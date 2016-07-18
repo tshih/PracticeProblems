e8a0481ecdbd596568a2b48e24192a1ddcf85784
Timothy Shih <timothy.ts.shih@gmail.com>
2016-07-18 00:24:30 -0500

    commit 2.2

--- testsrc/chapter2/P2KthToLastTest.java
+++ testsrc/chapter2/P2KthToLastTest.java
@@ -22,12 +22,12 @@ public class P2KthToLastTest {
 
     @Test
     public void getKthFromEndTest() throws Exception {
-        int[] data = {1,2,3,4,5,6,7,8,9,10};
-        Node first = Node.createLinkedList(data);
+        Integer[] data = {1,2,3,4,5,6,7,8,9,10};
+        Node<Integer> first = NodeUtils.createLinkedList(data);
 
         int k = 3;
-        int value = obj.getKthFromEnd(first ,k).getData();
-        assertEquals("Expected: " + 8 + " , Actual: " + value,8 ,value);
+        Integer value = (Integer) obj.getKthFromEnd(first ,k).getData();
+        assertEquals("Expected: " + 8 + " , Actual: " + value, (Integer) 8 ,value);
     }
 
 }
